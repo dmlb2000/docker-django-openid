df2cd854ef415c82c74c1cb2fb2562e1e2db5481
dmlb2000 <dmlb2000@gmail.com>
2015-10-31 16:40:53 -0400

    wrong if statement

--- runit.sh
+++ runit.sh
@@ -4,7 +4,7 @@ if ! test -e /app/.setup.stamp ; then
   # wait 10 seconds for mysql to show up
   i=0
   while test $i -lt 10 ; do
-    if mysql -h $MYSQL_PORT_3306_TCP_ADDR -u $MYSQL_ENV_MYSQL_USER -p $MYSQL_ENV_MYSQL_PASSWORD -P $MYSQL_PORT_3306_TCP_PORT $MYSQL_ENV_MYSQL_DATABASE 'select 1;'
+    if mysql -h $MYSQL_PORT_3306_TCP_ADDR -u $MYSQL_ENV_MYSQL_USER -p $MYSQL_ENV_MYSQL_PASSWORD -P $MYSQL_PORT_3306_TCP_PORT $MYSQL_ENV_MYSQL_DATABASE 'select 1;' ; then
       break
     fi
   done
